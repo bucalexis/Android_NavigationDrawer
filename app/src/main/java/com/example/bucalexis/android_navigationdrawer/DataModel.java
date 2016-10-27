2d0749ff0af5f3e3f9ea3c9b9e2e8107be1d755a
bucalexis <bucalexis@hotmail.com>
2016-10-26 21:43:34 -0500

    Initial commit

---
+++ app/src/main/java/com/example/bucalexis/android_navigationdrawer/DataModel.java
@@ -0,0 +1,18 @@
+package com.example.bucalexis.android_navigationdrawer;
+
+/**
+ * Created by NanX3 on 26/10/2016.
+ */
+
+public class DataModel {
+
+    public int icon;
+    public String name;
+
+    // Constructor.
+    public DataModel(int icon, String name) {
+
+        this.icon = icon;
+        this.name = name;
+    }
+}
